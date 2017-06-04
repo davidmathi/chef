d6b9591194dc79bfb8ca0c9ae7762c9ee9dae47b
davidmathi91 <davidlivingstone.mathi@gmail.com>
2017-06-04 07:54:44 +0000

    final

--- cookbooks/apache/recipes/websites.rb
+++ cookbooks/apache/recipes/websites.rb
@@ -1,5 +1,10 @@
 file 'default www' do
       path '/var/www/html/index.html'
-      content "Hello world!"
+      content "Hello world! for staging"
 end
 
+webnodes=search('node','role:web')
+
+webnodes.each do |node|
+        puts node
+end
